612118593bcf15b925ae2b1b6962b644f147006b
erneto13 <erneto.amaral13@gmail.com>
2024-09-21 18:31:28 -0700

    Initial commit

---
+++ src/main/java/com/erneto13/sgfa_backend/SgfaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.erneto13.sgfa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgfaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erneto13/sgfa_backend/SgfaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erneto13.sgfa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
