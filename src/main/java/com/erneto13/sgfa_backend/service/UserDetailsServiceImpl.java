16f4bbabd9bd030de575311e995086aa2e908115
erneto13 <erneto.amaral13@gmail.com>
2024-11-28 16:43:38 -0700

    update: se eliminó el que se pueda manejar el perfil del conductor asociado al user que inicia sesión (cambio urgente)

--- src/main/java/com/erneto13/sgfa_backend/service/UserDetailsServiceImpl.java
+++ src/main/java/com/erneto13/sgfa_backend/service/UserDetailsServiceImpl.java
@@ -1,12 +1,8 @@
 package com.erneto13.sgfa_backend.service;
 
-import com.erneto13.sgfa_backend.model.DriverModel;
 import com.erneto13.sgfa_backend.model.UserModel;
-import com.erneto13.sgfa_backend.repository.IDriverRepository;
 import com.erneto13.sgfa_backend.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,53 +10,22 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-
     @Autowired
     private IUserRepository iUserRepository;
 
-    @Autowired
-    private IDriverRepository iDriverRepository;
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        // Call Database to validate
+
         UserModel userModel = this.iUserRepository.findByEmail(email);
         if (userModel == null) {
             throw new UsernameNotFoundException(email);
         }
-
-        // Get driver information
-        DriverModel driverInfo = this.iDriverRepository.findByUserId(userModel.getUser_id());
-
-        // Create authorities list
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(userModel.getRole()));
-
-        return new CustomUserDetails(
+        return new User(
                 userModel.getEmail(),
                 userModel.getPassword(),
-                authorities,
-                driverInfo
-        );
-    }
-
-    public class CustomUserDetails extends User {
-        private final DriverModel driverInfo;
-
-        public CustomUserDetails(String username, String password,
-                                 Collection<? extends GrantedAuthority> authorities,
-                                 DriverModel driverInfo) {
-            super(username, password, authorities);
-            this.driverInfo = driverInfo;
-        }
-
-        public DriverModel getDriverInfo() {
-            return driverInfo;
-        }
+                new ArrayList<>());
     }
 }
