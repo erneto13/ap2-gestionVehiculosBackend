5c48e70f8f3be167cd13f0a0e85fa8ca15f97710
erneto13 <erneto.amaral13@gmail.com>
2024-11-28 16:40:35 -0700

    update: mejor manejo de rutas y métodos CRUD para los conductores

--- src/main/java/com/erneto13/sgfa_backend/controller/DriverController.java
+++ src/main/java/com/erneto13/sgfa_backend/controller/DriverController.java
@@ -1,27 +1,48 @@
 package com.erneto13.sgfa_backend.controller;
 
 import com.erneto13.sgfa_backend.model.DriverModel;
-import com.erneto13.sgfa_backend.service.UserDetailsServiceImpl;
+import com.erneto13.sgfa_backend.service.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@CrossOrigin("*")
-@RequestMapping("api/v1")
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/drivers")
+@CrossOrigin(origins = "*")
 public class DriverController {
 
-    @GetMapping("driver/profile")
-    public ResponseEntity<?> getProfile(Authentication authentication) {
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetailsServiceImpl.CustomUserDetails) {
-            UserDetailsServiceImpl.CustomUserDetails userDetails = (UserDetailsServiceImpl.CustomUserDetails) authentication.getPrincipal();
-            DriverModel driverInfo = userDetails.getDriverInfo();
-            return ResponseEntity.ok(driverInfo);
+    @Autowired
+    private DriverService driverService;
+
+    @GetMapping("drivers-list")
+    public ResponseEntity<List<DriverModel>> getAllDrivers() {
+        List<DriverModel> drivers = driverService.getAllDrivers();
+        return new ResponseEntity<>(drivers, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<DriverModel> createDriver(@RequestBody DriverModel driver) {
+        DriverModel newDriver = driverService.saveDriver(driver);
+        return new ResponseEntity<>(newDriver, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DriverModel> updateDriver(@PathVariable Long id, @RequestBody DriverModel driver) {
+        DriverModel updatedDriver = driverService.updateDriver(id, driver);
+        if (updatedDriver != null) {
+            return new ResponseEntity<>(updatedDriver, HttpStatus.OK);
         }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDriver(@PathVariable Long id) {
+        driverService.deleteDriver(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
