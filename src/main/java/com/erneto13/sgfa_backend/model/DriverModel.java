643a0799b4a8a291aa4089f1003ea2a4c9482f53
erneto13 <erneto.amaral13@gmail.com>
2024-11-28 16:41:13 -0700

    update: mejor manejo de excepciones y servioc con JpaRepository

--- src/main/java/com/erneto13/sgfa_backend/model/DriverModel.java
+++ src/main/java/com/erneto13/sgfa_backend/model/DriverModel.java
@@ -1,14 +1,28 @@
 package com.erneto13.sgfa_backend.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "drivers")
 public class DriverModel {
-    private Integer iddrivers;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long driver_id;
+
+    @Column
     private String name;
+
+    @Column
     private String license_number;
+
+    @Column
+    private String license_category;
+
+    @Column
     private String phone;
-    private String email;
+
+    @Column
     private String address;
-    private Integer user_id;
 }
