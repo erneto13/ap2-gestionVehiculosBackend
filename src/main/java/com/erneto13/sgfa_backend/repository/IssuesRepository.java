c8dcb3d099ff737b3f128380e75d9bacbd1c97c1
erneto13 <erneto.amaral13@gmail.com>
2024-11-28 16:45:33 -0700

    update: mejor manejo de peticines con más parametros especiales

--- src/main/java/com/erneto13/sgfa_backend/repository/IssuesRepository.java
+++ src/main/java/com/erneto13/sgfa_backend/repository/IssuesRepository.java
@@ -17,32 +17,37 @@ public class IssuesRepository implements IIssuesRepository {
 
     @Override
     public List<IssueModel> findAll() {
-        String query = "SELECT * FROM issues";
-        return jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(IssueModel.class));
+        String query = """
+                SELECT 
+                    idissues, title, description, issue_type, status,
+                    evidence, comments, reported_by, report_date, resolved_by, resolved_date 
+                FROM issues
+                """;
+        return jdbcTemplate.query(query, new IssueRowMapper());
     }
 
     @Override
     public List<IssueModel> findByIssueType(String type) {
         String query = "SELECT * FROM issues WHERE issue_type = ? AND status != 'closed'";
-        return jdbcTemplate.query(query, new Object[]{type}, BeanPropertyRowMapper.newInstance(IssueModel.class));
+        return jdbcTemplate.query(query, new IssueRowMapper(), type);
     }
 
     @Override
     public List<IssueModel> findByStatus(String status) {
         String query = "SELECT * FROM issues WHERE status = ?";
-        return jdbcTemplate.query(query, new Object[]{status}, BeanPropertyRowMapper.newInstance(IssueModel.class));
+        return jdbcTemplate.query(query, new IssueRowMapper(), status);
     }
 
     @Override
     public List<IssueModel> findByIssueTypePerUser(String type, String user) {
         String query = "SELECT * FROM issues WHERE issue_type = ? AND reported_by = ? AND (status = 'pending' OR status = 'in_progress')";
-        return jdbcTemplate.query(query, new Object[]{type, user}, BeanPropertyRowMapper.newInstance(IssueModel.class));
+        return jdbcTemplate.query(query, new IssueRowMapper(), type, user);
     }
 
     @Override
     public List<IssueModel> findClosedIssuesByUser(String user) {
         String query = "SELECT * FROM issues WHERE reported_by = ? AND status = 'closed'";
-        return jdbcTemplate.query(query, new Object[]{user}, BeanPropertyRowMapper.newInstance(IssueModel.class));
+        return jdbcTemplate.query(query, new IssueRowMapper(), user);
     }
 
     @Override
@@ -92,19 +97,6 @@ public class IssuesRepository implements IIssuesRepository {
     @Override
     public IssueModel findById(int id) {
         String sql = "SELECT * FROM issues WHERE idissues = ?";
-        return jdbcTemplate.queryForObject(sql, new Object[]{id}, (rs, rowNum) -> {
-            IssueModel issue = new IssueModel();
-            issue.setIdissues(rs.getInt("idissues"));
-            issue.setTitle(rs.getString("title"));
-            issue.setDescription(rs.getString("description"));
-            issue.setIssue_type(rs.getString("issue_type"));
-            issue.setStatus(rs.getString("status"));
-            issue.setComments(rs.getString("comments"));
-            issue.setReportedBy(rs.getString("reported_by"));
-            issue.setReportDate(rs.getTimestamp("report_date").toLocalDateTime());
-            issue.setResolvedBy(rs.getString("resolved_by"));
-            issue.setResolvedDate(rs.getTimestamp("resolved_date") != null ? rs.getTimestamp("resolved_date").toLocalDateTime() : null);
-            return issue;
-        });
+        return jdbcTemplate.queryForObject(sql, new IssueRowMapper(), id);
     }
 }
