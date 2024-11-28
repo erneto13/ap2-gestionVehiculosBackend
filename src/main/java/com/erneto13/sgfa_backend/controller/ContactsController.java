2862cccd66a7de26cbd8eb9030d212f12bc32ffd
erneto13 <erneto.amaral13@gmail.com>
2024-11-28 16:38:18 -0700

    fix: ajuste de rutas para un mejor entendimiento y creación de los demás métodos CRUD

--- src/main/java/com/erneto13/sgfa_backend/controller/ContactsController.java
+++ src/main/java/com/erneto13/sgfa_backend/controller/ContactsController.java
@@ -1,29 +1,42 @@
 package com.erneto13.sgfa_backend.controller;
 
 import com.erneto13.sgfa_backend.model.ContactModel;
+import com.erneto13.sgfa_backend.model.RemindersModel;
 import com.erneto13.sgfa_backend.service.IContactsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @CrossOrigin("*")
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/contacts")
 public class ContactsController {
 
     @Autowired
     IContactsService iContactsService;
 
-    @GetMapping("/contact-list")
-    public ResponseEntity<?> list() {
-        List<ContactModel> contacts = this.iContactsService.findAll();
-        return new ResponseEntity<>(contacts, HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<List<ContactModel>> getAllContacts() {
+        return new ResponseEntity<>(iContactsService.getAllContacts(), HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ContactModel> getContactById(@PathVariable Long id) {
+        return new ResponseEntity<>(iContactsService.getContactById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ContactModel> createContact(@RequestBody ContactModel contact) {
+        return new ResponseEntity<>(iContactsService.createContact(contact), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteContact(@PathVariable Long id) {
+        iContactsService.deleteContact(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
