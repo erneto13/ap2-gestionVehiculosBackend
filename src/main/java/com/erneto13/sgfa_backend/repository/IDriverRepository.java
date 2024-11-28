00e6b4f74740bbcb31b601ddd21c5b4d5c669e55
erneto13 <erneto.amaral13@gmail.com>
2024-11-28 16:42:15 -0700

    delete: en desuso

--- src/main/java/com/erneto13/sgfa_backend/repository/IDriverRepository.java
+++
@@ -1,7 +0,0 @@
-package com.erneto13.sgfa_backend.repository;
-
-import com.erneto13.sgfa_backend.model.DriverModel;
-
-public interface IDriverRepository {
-    DriverModel findByUserId(Integer userId);
-}
