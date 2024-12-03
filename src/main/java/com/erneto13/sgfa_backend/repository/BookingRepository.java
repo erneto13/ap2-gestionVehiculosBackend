6a524cbf439d9ffaddb098e3ac2759a01adf7495
erneto13 <erneto.amaral13@gmail.com>
2024-12-03 16:08:49 -0700

    update: nuevo método para encontrar un vehículo por su id

--- src/main/java/com/erneto13/sgfa_backend/repository/BookingRepository.java
+++ src/main/java/com/erneto13/sgfa_backend/repository/BookingRepository.java
@@ -1,6 +1,7 @@
 package com.erneto13.sgfa_backend.repository;
 
 import com.erneto13.sgfa_backend.model.BookingModel;
+import com.erneto13.sgfa_backend.model.VehicleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<BookingModel, Long> {
+    List<BookingModel> findByVehicle(VehicleModel vehicle);
 }
