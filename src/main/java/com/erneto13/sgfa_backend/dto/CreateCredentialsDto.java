f28745965f12bb42dbe3af35d310b85954064d25
erneto13 <erneto.amaral13@gmail.com>
2024-12-04 18:56:44 -0700

    feature: modelo y servicio para notificarle al conductor de las credenciales

---
+++ src/main/java/com/erneto13/sgfa_backend/dto/CreateCredentialsDto.java
@@ -0,0 +1,12 @@
+package com.erneto13.sgfa_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateCredentialsDto {
+    private Long driverId;
+    private String email;
+    private String password;
+
+
+}
