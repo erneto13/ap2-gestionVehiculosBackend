a76c53da5041f984d47e945495c707cc325cfb6d
erneto13 <erneto.amaral13@gmail.com>
2024-12-07 13:48:44 -0700

    feat(config): se agregó la anotación @EnableAsync a WebConfig

--- src/main/java/com/erneto13/sgfa_backend/config/WebConfig.java
+++ src/main/java/com/erneto13/sgfa_backend/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.erneto13.sgfa_backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableAsync
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
